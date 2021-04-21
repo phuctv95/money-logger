1f1f694ddefa1736fe7bf78a4753f6bc40a1b652
phuc <phuctv95@gmail.com>
2021-04-21 17:37:30 +0700

    add routing animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
+import { animate, animateChild, group, query, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('routeAnimation', [
+      transition('* <=> *', [
+        style({
+          position: 'relative',
+        }),
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%'
+          })
+        ]),
+        query(':enter', [
+          style({ opacity: 0 })
+        ]),
+        query(':leave', animateChild()),
+        query(':leave', [
+          animate('500ms', style({ opacity: 0 }))
+        ]),
+        query(':enter', [
+          animate('500ms', style({ opacity: 1 }))
+        ]),
+        query(':enter', animateChild()),
+      ]),
+    ]),
+  ]
 })
 export class AppComponent {
+
+  getState(outlet: RouterOutlet) {
+    console.log(outlet.activatedRouteData.depth);
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.depth;
+  }
   
 }
--- src/app/modules/money-logger/money-logger-routing.module.ts
+++ src/app/modules/money-logger/money-logger-routing.module.ts
@@ -4,8 +4,8 @@ import { StartPageComponent } from './components/start-page/start-page.component
 import { TodayLoggerComponent } from './components/today-logger/today-logger.component';
 
 const routes: Routes = [
-  { path: '', component: StartPageComponent },
-  { path: 'today', component: TodayLoggerComponent },
+  { path: '', component: StartPageComponent, data: { depth: 1 } },
+  { path: 'today', component: TodayLoggerComponent, data: { depth: 2 } },
 ];
 
 @NgModule({
